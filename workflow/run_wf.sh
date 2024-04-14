bfc284a4e365f20e5c51d022d69982a35fe28d37
dariushghasemi <ghasemi.dariush@yahoo.com>
2024-04-14 23:54:14 +0200

    start integrating into pipeline!

---
+++ workflow/run_wf.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+#SBATCH --job-name worker
+#SBATCH --output %j_test_s03.log
+#SBATCH --partition cpuq
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 8G
+#SBATCH --time 8:00:00
+
+#    --input test_pwas.regenie.tsv.gz  \
+pwas=/exchange/healthds/pQTL/results/INTERVAL_OLD/harmonized_sumstats/outputs/seq.7930.3/seq.7930.3.regenie.tsv.gz
+
+#RAM needed: 16GB
+
+# Rscript s01_sumstat_munging.R  \
+#     --pipeline_path /home/dariush.ghasemi/projects/pqtl_pipeline_finemap/workflow/scripts/  \
+#     --input /exchange/healthds/pQTL/results/INTERVAL_OLD/harmonized_sumstats/outputs/seq.7930.3/seq.7930.3.regenie.tsv.gz  \
+#     --is_molQTL FALSE  \
+#     --type quant  \
+#     --s NA  \
+#     --study_id seq.7930.3
+
+
+# Rscript s02_sumstat_alignment.R  \
+#     --pipeline_path /home/dariush.ghasemi/projects/pqtl_pipeline_finemap/workflow/scripts/  \
+#     --dataset seq.7930.3_dataset.rds  \
+#     --study_id seq.7930.3  \
+#     --grch 38  \
+#     --chr_tabix 22
+
+
+Rscript s03_locus_breaker.R  \
+    --pipeline_path /home/dariush.ghasemi/projects/pqtl_pipeline_finemap/workflow/scripts/  \
+    --study_id seq.7930.3  \
+    --p_thresh1 5e-06  \
+    --p_thresh2 1e-04
